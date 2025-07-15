45b341bc4b85ca0c84c7ca7ac68e0bf01eccff23
Adarsh Danda <adarsh.danda1@gmail.com>
2025-07-15 06:05:12 -0700

    added network req capture in this commit

---
+++ background.js
@@ -0,0 +1,28 @@
+// Listen for all network requests
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+      // Log the request details to the console
+      if (details.url.includes("https://us-central1-neetcode-dd170.cloudfunctions.net/runCodeFunction") || details.url.includes("https://us-central1-neetcode-dd170.cloudfunctions.net/executeCodeFunction")) {
+        const requestBody = details.requestBody;
+        const buffer = requestBody.raw[0].bytes;
+        const uint8Array = new Uint8Array(buffer);
+        const decoder = new TextDecoder('utf-8');
+        const decodedString = decoder.decode(uint8Array);
+        const data = JSON.parse(decodedString);
+        const title = data.data.problemId;
+        const code = data.data.rawCode;
+        console.log(title);
+        console.log('________________________________________________________');
+        console.log(code);
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                type: 'CODE_DATA',
+                title: title,
+                code: code
+            });
+        });
+      }
+    },
+    { urls: ["https://us-central1-neetcode-dd170.cloudfunctions.net/*"] },
+    ["requestBody"]
+);
